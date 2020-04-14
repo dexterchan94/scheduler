c83345cbc3e45ff36d4b35ec28ccda6395d2d652
Dexter Chan <dexter@Dexters-MacBook-Air.local>
2020-04-14 13:42:16 -0700

    add appointment, header, empty, and show components

--- stories/index.js
+++ stories/index.js
@@ -5,11 +5,16 @@ import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
+
 import Button from "components/Button";
 import DayList from "components/DayList";
 import DayListItem from "components/DayListItem";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -133,3 +138,22 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+  <Show
+    student="Lydia Miller-Jones"
+    interviewer={interviewers[0]}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+  />))
+  ;
