ff8680f8c5133fa075a2cc08a55fbc13007bc880
Dexter Chan <dexter@Dexters-MacBook-Air.local>
2020-04-14 13:55:17 -0700

    add ErrorComponent component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import ErrorComponent from "components/Appointment/ErrorComponent";
 
 storiesOf("Button", module)
   .addParameters({
@@ -166,4 +167,9 @@ storiesOf("Appointment", module)
     />
   ))
   .add("Status", () => <Status message="Deleting" />)
+  .add("ErrorComponent", () => (
+    <ErrorComponent
+      message="Could not delete appointment"
+      onClose={action("onClose")}
+    />))
   ;
